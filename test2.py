0f332b3443fe8388842996709161c17dda6055f2
Alexandros Antoniadis Karnavas <aantoniadis@localhost.localdomain>
2017-10-20 13:05:46 +0100

    First commit for test

---
+++ test2.py
@@ -0,0 +1,21 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+
+hdulist = fits.open('vesta_harps_comb_rv.fits')
+
+print(hdulist)
+hdu = hdulist[0]
+crval = hdu.header['CRVAL1']
+
+cdel = hdu.header['CDELT1']
+
+size = hdu.header['NAXIS1']
+
+wavelength = crval + cdel * np.arange(size)
+
+plt.xlabel("wavelength")
+plt.ylabel("flux")
+
+plt.plot(wavelength, hdulist[0].data)
+plt.show()
